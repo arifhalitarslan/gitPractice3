2accd64b1557961572e0e8617fafe6e9a56fab0a
arifhalitarslan <arifhalitarslan0@gmail.com>
2022-04-14 09:56:21 +0300

    Pyramid has been created.

--- src/practice1.java
+++ src/practice1.java
@@ -2,7 +2,12 @@ public class practice1 {
 
     public static void main(String[] args) {
 
-
+        for (int i = 1; i < 5; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
 
 
 
